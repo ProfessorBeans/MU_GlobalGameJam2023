ba1007e4cd4db5035a63cd4301854b16f2c5d4f2
Prof Beans <professorbeans98@gmail.com>
2023-02-02 21:07:55 -0600

    Completed Node functionality, working on Vine Functionality.

---
+++ MUGGJ2023/Assets/Code/MainCamControls.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamControls : MonoBehaviour
+{
+    //State Tracking
+    public bool isLookingRoots;
+
+    private void Start()
+    {
+        isLookingRoots = false;
+    }
+
+    public void PanToRoots()
+    {
+        isLookingRoots = true;
+        this.GetComponent<Transform>().Translate(0, -6, 0);
+    }
+
+    public void PanToSeedBoy()
+    {
+        isLookingRoots = false;
+        this.GetComponent<Transform>().Translate(0, 6, 0);
+    }
+}
--- MUGGJ2023/Assets/Code/NodeControls/HolderController.cs
+++ MUGGJ2023/Assets/Code/NodeControls/HolderController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class HolderController : MonoBehaviour
@@ -8,13 +9,26 @@ public class HolderController : MonoBehaviour
     public GameObject node1;
     public GameObject node2;
     public GameObject node3;
+    public GameObject _snake;
 
+    //PLANT MUST BE SET IN INSPECTOR
+    public GameObject _plant;
+    
+    public void ResetNodes()
+    {
+        node1.GetComponent<NodeController>().isOn = false;
+        node2.GetComponent<NodeController>().isOn = false;
+        node3.GetComponent<NodeController>().isOn = false;
+    }
+    
     // Update is called once per frame
     void Update()
     {
         if (node1.GetComponent<NodeController>().isOn == true && node2.GetComponent<NodeController>().isOn && node3.GetComponent<NodeController>().isOn)
         {
             print("Puzzle solved.");
+            _plant.GetComponent<SpriteRenderer>().enabled = true;
+            _snake.GetComponent<SnakeMovement>().isActive = false;
         }
     }
 }
